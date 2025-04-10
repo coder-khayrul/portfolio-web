85f06d51fc9fbba527ec38aa28b483e7cb09b531
khayrul200822 <khayrulislam200822@gmail.com>
2025-04-10 21:56:52 +0600

    tesimonial section added

--- src/js/script.js
+++ src/js/script.js
@@ -41,3 +41,28 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
+
+//**Hanlde Swiper Slider */
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
