6257467f915b692c6b5b71f48ed548d9c1ef6acf
khayrul200822 <khayrulislam200822@gmail.com>
2025-04-14 23:53:55 +0600

    add lazy loading

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -14,7 +14,7 @@ const handlePortfolioItems = (Items) => {
       child.innerHTML=`
          <div class="p-5 bg-white rounded-lg mb-4 border border-[#f4f5f7] hover:shadow-badge duration-700">
               <div class="border-2 p-2 border-[#593DAB] rounded-lg overflow-hidden group">
-                <img class="group-hover:scale-[1.2] group-hover:rotate-12 duration-700" src="${item.link}" alt="">
+                <img class="group-hover:scale-[1.2] group-hover:rotate-12 duration-700" loading="lazy" src="${item.link}" loa alt="">
               </div>
               <div class="pt-4">
                 <h3 class="text-[18px] font-semibold mb-2">${item.title}</h3>
