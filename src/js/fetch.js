fbf8749639d697a363a33dfdc150c6f43bcb67c3
khayrul200822 <khayrulislam200822@gmail.com>
2025-04-14 23:20:41 +0600

    new section added

---
+++ src/js/fetch.js
@@ -0,0 +1,28 @@
+
+//**Portfolio Data */
+
+const portfolioContainer = document.getElementById("portfolio-container")
+
+ fetch("/portfolio.json")
+  .then(res => res.json())
+  .then(data => handlePortfolioItems(data));
+
+const handlePortfolioItems = (Items) => {
+ 
+    Items.map(item => {
+      const child = document.createElement("div");
+      child.innerHTML=`
+         <div class="p-5 bg-white rounded-lg mb-4 border border-[#f4f5f7] hover:shadow-badge duration-700">
+              <div class="border-2 p-2 border-[#593DAB] rounded-lg overflow-hidden group">
+                <img class="group-hover:scale-[1.2] group-hover:rotate-12 duration-700" src="${item.link}" alt="">
+              </div>
+              <div class="pt-4">
+                <h3 class="text-[18px] font-semibold mb-2">${item.title}</h3>
+                <span class="text-[16px] text-[#999]">${item.category}</span>
+              </div>
+            </div>
+      `
+      portfolioContainer.appendChild(child);
+    })
+}
+
