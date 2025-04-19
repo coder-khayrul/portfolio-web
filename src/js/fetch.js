148c3e5fbb0c53b99c8c80ee7d498e70be76fe20
khayrul200822 <khayrulislam200822@gmail.com>
2025-04-19 22:24:27 +0600

    portfolio filter by category features added

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -2,28 +2,59 @@
 //**Portfolio Data */
 
 const portfolioContainer = document.getElementById("portfolio-container")
-
- fetch("portfolio.json")
+const portfolioCategories = document.querySelectorAll(".portfolio-categories li")
+const modalItems = document.querySelectorAll(".openModal")
+//handling default category display
+fetch("portfolio.json")
   .then(res => res.json())
-  .then(data => handlePortfolioItems(data));
+  .then(data => handleItemAppend(data));
 
-const handlePortfolioItems = (Items) => {
- 
-    Items.map(item => {
-      const child = document.createElement("div");
-      child.innerHTML=`
-         <div class="p-5 bg-white rounded-lg mb-4 border border-[#f4f5f7] hover:shadow-badge duration-700">
-              <div class="border-2 p-2 border-[#593DAB] rounded-lg overflow-hidden group">
-               <img class="group-hover:scale-[1.2] group-hover:rotate-12 duration-700" loading="lazy"
-                  src="${item.image}" loading="lazy" alt="">
-              </div>
-              <div class="pt-4">
-                <h3 class="text-[18px] font-semibold mb-2">${item.title}</h3>
-                <span class="text-[16px] text-[#999]">${item.category}</span>
-              </div>
-            </div>
-      `
-      portfolioContainer.appendChild(child);
+//-----------/
+
+//handling category filter
+portfolioCategories.forEach((category) => {
+  category.addEventListener("click", (e) => {
+    portfolioCategories.forEach((category) => {
+      category.classList.remove("current");
     })
-}
+    portfolioContainer.innerHTML = "";
+    category.classList.add("current");
+    const currentCateogoryId = e.target.getAttribute("data-category")
+    fetch("portfolio.json")
+      .then(res => res.json())
+      .then(data => handlePortfolioItems(data, currentCateogoryId));
+  })
+  
+})
+//_____________//
 
+
+//handling all portfolio items from json file
+const handlePortfolioItems = (allItems, categoryId) => {
+  const categoryItems = allItems.filter(item => {
+    return item.category_slugs.includes(categoryId)
+  })
+  handleItemAppend(categoryItems)
+}
+//------------//
+
+//add items to the portfolio container
+const handleItemAppend = (items) => {
+  items.map(item => {
+    const child = document.createElement("div");
+    child.innerHTML = `
+       <div class="p-5 bg-white rounded-lg mb-4 border border-[#f4f5f7] hover:shadow-badge duration-700">
+            <div class="openModal cursor-pointer border-2 p-2 border-[#593DAB] rounded-lg overflow-hidden group">
+             <img class="group-hover:scale-[1.2] group-hover:rotate-12 duration-700" loading="lazy"
+                src="${item.image}" loading="lazy" alt="">
+            </div>
+            <div class="pt-4">
+              <h3 class="text-[18px] font-semibold mb-2">${item.title}</h3>
+              <span class="text-[16px] text-[#999]">${item.category_name}</span>
+            </div>
+          </div>
+    `
+    portfolioContainer.appendChild(child);
+  })
+}
+//------------//
